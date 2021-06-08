fa4eff24d11aff2180f77bb60b3c8479c66432c6
Cynthia Dev <cynthiaaidev@gmail.com>
2021-06-08 15:23:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 
 var bot = new Discord.Client();
 
-var prefix = ("!");
-bot.login("Your token here");
+var prefix = ("c!");
+bot.login("token");
 
 bot.on('ready', () => {
-	console.log("Global ready !");
+	console.log("Serverlist ready!");
 });
 
 bot.on('message', message => {
@@ -16,46 +16,73 @@ bot.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
 	if (message.content.startsWith(prefix + "serverlist")) {
-		(async function () {
-			let p0 = 0;
-			let p1 = 10;
-			const data = await message.channel.send(bot.guilds.cache.sort((a, b) => b.memberCount - a.memberCount).map(r => r).map((r, i) => `[${i + 1}] • Name: \`${r.name.toString()}\`\nUsers: \`${r.memberCount}\`\nID: \`${r.id}\``).slice(0, 10).join('\n'));
-			await data.react("⬅");
-			await data.react("➡");
-			await data.react("❌");
-			const data_res = data.createReactionCollector((reaction, user) => user.id === message.author.id);
-			data_res.on("collect", async (reaction) => {
-				if (reaction.emoji.name === "⬅") {
-					p0 = p0 - 10;
-					p1 = p1 - 10;
-					if (p0 < 0) return data.delete();
-					if (p0 === undefined || p1 === undefined) return data.delete();
-					data.edit(bot.guilds.sort((a, b) => b.memberCount - a.memberCount).map(r => r).map((r, i) => `[${i + 1}] • Name: \`${r.name.toString()}\`\nUsers: \`${r.memberCount}\`\nID: \`${r.id}\``).slice(p0, p1).join('\n'));
-				}
-				if (reaction.emoji.name === "➡") {
-					p0 = p0 + 10;
-					p1 = p1 + 10;
-					if (p1 > bot.guilds.size + 10) return data.delete();
-					if (p0 === undefined || p1 === undefined) return data.delete();
-					data.edit(bot.guilds.sort((a, b) => b.memberCount - a.memberCount).map(r => r).map((r, i) => `[${i + 1}] • Name: \`${r.name.toString()}\`\nUsers: \`${r.memberCount}\`\nID: \`${r.id}\``).slice(p0, p1).join('\n'));
-				}
-				if (reaction.emoji.name === "❌") {
-					return data.delete(data);
-				}
-				await reaction.remove(message.author.id);
-			})
-		}())
-	}
-	if (message.content.startsWith(prefix + "invitestaff")) {
-		const arg = args.slice(1).join(" ");
+		const guilds = bot.guilds.cache.array().sort((a, b) => b.memberCount - a.memberCount)
 
+/**
+ * 
+ * @param {number} start
+ */
+const generateEmbed = start => {
+  const current = guilds.slice(start, start + 5)
+
+  // you can of course customise this embed however you want
+  const embed = new Discord.MessageEmbed()
+    .setTitle(`Showing guilds ${start + 1}-${start + current.length} out of ${guilds.length}`)
+	.setColor(0x206694)
+  current.forEach(g => embed.addField(g.name, `**ID:** ${g.id}
+**Members**: ${g.memberCount}
+**Owner:** ${g.owner.user.tag}`))
+embed.addField("To join a Guild use:", "`c!get-invite <ID>`")
+  return embed
+}
+
+
+const author = message.author
+
+// send the embed with the first 5 guilds
+message.channel.send(generateEmbed(0)).then(message => {
+  // exit if there is only one page of guilds (no need for all of this)
+  if (guilds.length <= 5) return
+  // react with the right arrow (so that the user can click it) (left arrow isn't needed because it is the start)
+  message.react('➡️')
+  const collector = message.createReactionCollector(
+    // only collect left and right arrow reactions from the message author
+    (reaction, user) => ['⬅️', '➡️'].includes(reaction.emoji.name) && user.id === author.id,
+    // time out after a minute
+    {time: 60000}
+  )
+
+  let currentIndex = 0
+  collector.on('collect', reaction => {
+    // remove the existing reactions
+    message.reactions.removeAll().then(async () => {
+      // increase/decrease index
+      reaction.emoji.name === '⬅️' ? currentIndex -= 5 : currentIndex += 5
+      // edit message with new embed
+      message.edit(generateEmbed(currentIndex))
+      // react with left arrow if it isn't the start (await is used so that the right arrow always goes after the left)
+      if (currentIndex !== 0) await message.react('⬅️')
+      // react with right arrow if it isn't the end
+      if (currentIndex + 5 < guilds.length) message.react('➡️')
+    })
+  })
+})
+}
+	//embed-ized
+	if (message.content.startsWith(prefix + "get-invite")) {
+		const arg = args.slice(1).join(" ");
 		let sv = bot.guilds.cache.get(arg)
 		if (!sv) {
-			return message.channel.send(`Enter a valid guild id`);
+			return message.channel.send(`Enter a valid guild ID`);
 		} else {
-			sv.channels.cache.random().createInvite({
-				maxAge: 0
-			}).then(a => message.author.send("Invite link : " + a.toString()))
-		}
+			sv.channels.cache.random().createInvite({maxAge: 0})
+			.then( a =>
+			embed = new Discord.MessageEmbed()
+			.setTitle(`Here is your invite for "${sv.name}"`)
+			.setColor(0x206694)
+			.setThumbnail('https://s6.gifyu.com/images/emoji_84.gif')
+			.addField("Invite link : " + a.toString(), `Have fun in there and dont forget: Cynthia Eternal!`)
+			).then(embed => message.author.send(embed))
+			}
 	}
 });
